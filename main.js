3c25aa38542ef000870bd33478fa63a77fad3833
Rubi178 <78242301+Rubi178@users.noreply.github.com>
2021-04-19 20:58:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Next_page()
+{
+    window.location = "first_page.html";
+}
+
+function second_page()
+{
+    window.location = "second_page.html";
+}
+
+function third_page()
+{
+    window.location = "third_page.html";
+}
+
+function fourth_page()
+{
+    window.location = "fourth_page.html";
+}
